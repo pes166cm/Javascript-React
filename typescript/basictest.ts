72615183d3a227c7f10e050fb4437bff8cc76079
pes166cm <pesgp0507@gmail.com>
2024-06-10 18:07:26 +0900

    init

---
+++ typescript/basictest.ts
@@ -0,0 +1,38 @@
+var test="안녕"; //동적타입 
+
+console.log(test);
+//변수 선언 
+//let 변수명 : 자료형
+let studentName:string;
+
+studentName = "호기";
+//studentName = 19;
+console.log(studentName);
+let studentAge:number;
+//number, string, boolean, object <= 자료형[]
+let isTrue:boolean;
+let student:object; //객체형 타입이 모두 저장 가능함
+let hobby:string[];
+
+//any타입 권장하지 않음!!
+
+isTrue=true;
+isTrue=false;
+//isTrue=1;
+
+//함수선언하기
+//function 함수명(매개변수명:자료형,매개변수명:자료형....):반환형{}
+//const test = function  함수명(매개변수명:자료형,매개변수명:자료형....):반환형{}
+//const arrow=(매개변수명:자료형,매개변수명:자료형....):반환형{})
+//void 반환이 없을 때 사용
+
+import {test1, test2} from './mymodule/functionModule';
+test1("박은서", 19);
+console.log(test2(["하나","둘","셋"]));
+test1(19,"박은서");
+
+//solidity 언어
+
+
+
+
